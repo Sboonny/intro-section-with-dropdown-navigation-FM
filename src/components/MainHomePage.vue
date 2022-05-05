02097c39f6eda34d9d785fcf21aa9ab744a12f05
Sboonny <88248797+Sboonny@users.noreply.github.com>
2022-05-05 14:28:17 +0200

    finished functionality of nav button

--- src/components/MainHomePage.vue
+++ src/components/MainHomePage.vue
@@ -1,55 +1,58 @@
 <template>
- <main class="grid">
-  <div class="main--content">
-  <h1>Make remote work</h1>
+  <main class="grid">
+    <div class="main--content">
+      <h1>Make remote work</h1>
 
-  <p>Get your team in sync, no matter your location. Streamline processes, 
-  create team rituals, and watch productivity soar.</p>
+      <p>Get your team in sync, no matter your location. Streamline processes,
+        create team rituals, and watch productivity soar.</p>
 
-  <button>Learn more</button>
-   <div class="img--container">
-    <img src="src/assets/client-databiz.svg" alt="data biz">
-    <img src="src/assets/client-audiophile.svg" alt="audio phile">
-    <img src="src/assets/client-meet.svg" alt="meet">
-    <img src="src/assets/client-maker.svg" alt="maker">
-   </div>
-  </div>
-  <picture >
-    <source class="home-img" media="(min-width: 45em)" srcset="src/assets/image-hero-desktop.png">
-  <img class="home-img" src="src/assets/image-hero-mobile.png" alt="Remote work">
-</picture>
-</main>
+      <button>Learn more</button>
+      <div class="img--container">
+        <img src="src/assets/client-databiz.svg" alt="data biz">
+        <img src="src/assets/client-audiophile.svg" alt="audio phile">
+        <img src="src/assets/client-meet.svg" alt="meet">
+        <img src="src/assets/client-maker.svg" alt="maker">
+      </div>
+    </div>
+    <picture>
+      <source class="home-img" media="(min-width: 45em)" srcset="src/assets/image-hero-desktop.png">
+      <img class="home-img" src="src/assets/image-hero-mobile.png" alt="Remote work">
+    </picture>
+  </main>
 </template>
 
 <style scoped>
- .grid {
-   display: grid;
-   gap: var(--gap, 2em);
-   grid-template-rows: min-content 1fr;
-   grid-template-areas: 
-           'HomeImage'
-           'HomeMain';
- }
+.grid {
+  display: grid;
+  gap: var(--gap, 2em);
+  grid-template-rows: min-content 1fr;
+  grid-template-areas:
+    'HomeImage'
+    'HomeMain';
 
- main {
-   font-family: 'Epilogue', sans-serif;
-   font-size: 18px;
-   justify-items: end;
-   align-items: center;
-   margin: 1em 3em;
- }
+}
 
- h1 {
-   font-size: 4em;
-   font-weight: 700;
- }
+main {
+  font-family: 'Epilogue', sans-serif;
+  font-size: 18px;
+  place-items: center;
+  margin: 1em;
+  width: 100vw;
+  overflow: auto;
+}
 
- p {
-   color: hsl(var(--gray));
-   font-weight: 500;
- }
+h1 {
+  font-size: 1.5em;
+  font-weight: 700;
+  color: black;
+}
 
-  button {
+p {
+  color: hsl(var(--gray));
+  font-weight: 500;
+}
+
+button {
   background-color: hsl(var(--black));
   color: hsl(var(--white));
   border: 0;
@@ -58,40 +61,49 @@
   padding: .5em .9em;
 }
 
- .main--content {
-   grid-area: HomeMain;
-   display: grid;
-   color: hsl(var(--black));
-   font-style: inherit;
-   gap: var(--gap, 2em);
- }
- 
- .img--container {
-   display: flex;
-   gap: var(--gap, 2em);
- }
+.main--content {
+  grid-area: HomeMain;
+  display: grid;
+  color: hsl(var(--black));
+  font-style: inherit;
+  gap: var(--gap, 2em);
+  place-items: center;
+}
 
- .img--container img {
-   min-width: 100px;
-   width: 5vw;
-   max-width: 150px;
-   height: 3vw;
- }
+.img--container {
+  display: flex;
+  gap: var(--gap, 2em);
+}
 
- .home-img {
-   min-width: 300px;
-   width: 30vw;
-   max-width: 700px;
-   grid-area: HomeImage;
- }
+.img--container img {
+  min-width: 50px;
+  width: 5vw;
+  max-width: 150px;
+  min-height: 25px;
+  height: 3vw;
+}
+
+.home-img {
+  min-width: 300px;
+  width: 30vw;
+  max-width: 700px;
+  grid-area: HomeImage;
+}
 
 @media (min-width: 45em) {
   .grid {
-   grid-template-columns: 1fr 1fr;
-   grid-template-areas: 'HomeMain'
-                        'HomeImag';
-   }
-
+    grid-template-columns: 1fr 1fr;
+    grid-template-areas: 'HomeMain'
+      'HomeImag';
   }
 
+  main {
+    justify-items: end;
+    align-items: center;
+    marginblock: 3em;
+  }
+  h1 {
+    font-size: 3em;
+  }
+}
 </style>
--- src/components/NavComponent.vue
+++ src/components/NavComponent.vue
@@ -2,47 +2,54 @@
   <nav>
     <ul class="flex" role="list">
       <img src="src/assets/logo.svg" alt="logo" class="logo" />
-      <li>
+      <button class="menu-moblie" @click="showDropbox()"></button>
+      <li v-show="dropboxDisplay">
         <details>
           <summary>Features</summary>
           <div class="card">
-            <li :key="i" v-for="(feature, i) in features">
-              <v-img :src="import(`../assests/${icons[i].icon}`)" />
-              {{ feature }}
-            </li>
+            <ol>
+              <li :key="i" v-for="(feature, i) in features">
+                <v-img :src="import(`../assests/${icons[i].icon}`)" />
+                {{ feature }}
+              </li>
+            </ol>
           </div>
         </details>
       </li>
-      <li>
+      <li v-show="dropboxDisplay">
         <details>
           <summary>Company</summary>
           <div class="card">
-            <li :key="i" v-for="(product, i) in company">
-              {{ product }}
-            </li>
+            <ol>
+              <li :key="i" v-for="(product, i) in company">
+                {{ product }}
+              </li>
+            </ol>
           </div>
         </details>
       </li>
-      <button>
-        <li>Careers</li>
-      </button>
-      <button>
-        <li>About</li>
-      </button>
-      <button class="login-button">
-        <li>Login</li>
-      </button>
-      <button class="register-button">
-        <li>Register</li>
-      </button>
+      <li v-show="dropboxDisplay">
+        <button>
+          Careers
+        </button>
+      </li>
+      <li v-show="dropboxDisplay">
+        <button>
+          About
+        </button>
+      </li>
+      <li v-show="dropboxDisplay">
+        <button class="login-button">
+          Login
+        </button>
+      </li>
+      <li v-show="dropboxDisplay">
+        <button class="register-button">
+          Register
+        </button>
+      </li>
     </ul>
   </nav>
-  <div class="dropbox" id="dropbox">
-    <ul class="flex"></ul>
-  </div>
-  <div class="dropbox" id="dropbox">
-    <ul class="flex"></ul>
-  </div>
 </template>
 
 <script lang="ts">
@@ -73,12 +80,20 @@ export default defineComponent({
         },
       ] as any,
       company: ["History", "Our Team", "Blog"] as Array<String>,
+      dropboxDisplay: true as boolean
     };
+  },
+  methods: {
+  showDropbox() {
+    this.dropboxDisplay = !this.dropboxDisplay
+  }
   },
   mounted() {
     this.company;
     this.features;
     this.icons;
+    this.dropboxDisplay;
+    this.showDropbox()
   },
 });
 </script>
@@ -89,6 +104,7 @@ export default defineComponent({
   height: 3vh;
   min-height: 30px;
 }
+
 .flex {
   display: flex;
   gap: var(--gap, 2em);
@@ -97,6 +113,25 @@ export default defineComponent({
 button {
   border: 0;
   background-color: transparent;
+}
+
+.menu-moblie {
+  display: block;
+  position: absolute;
+  z-index: 2000;
+  right: 1rem;
+  top: 1rem;
+  background-image: url(../assets/icon-menu.svg);
+  background-repeat: no-repeat;
+  background-position: center;
+  width: 1.5rem;
+  aspect-ratio: 1;
+  border: 0;
+  padding: .5em;
+}
+
+.mobile-nav-toggle[open] {
+  background-image: url(../assets/icon-close-menu.svg);
 }
 
 button {
@@ -121,6 +156,10 @@ li:focus {
   color: hsl(var(--black));
 }
 
+.card {
+  position: relative;
+}
+
 .register-button {
   border: 2px solid hsl(var(--gray));
   border-radius: 15px;
@@ -138,6 +177,7 @@ li:focus {
 
   .card {
     position: absolute;
+    box-shadow: 0px 0px 20px 1px hsl(var(--white));
   }
 
   button {
@@ -152,8 +192,13 @@ li:focus {
     align-items: center;
     flex-direction: column;
   }
+
   .login-button {
     margin-left: auto;
+  }
+
+  .menu-moblie {
+    display: none;
   }
 }
 </style>
